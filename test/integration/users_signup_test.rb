4e2338a538effd3ca7d7cc977ea1f007e1d7c641
Nicole Radford <nradford0171@gmail.com>
2015-03-17 10:01:02 -0600

    half way done with chpt 8

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              "foo",
                                password_confirmation: "bar" }
     end
+    assert_template 'users/new'
   end
      test "valid signup information" do
     get signup_path
@@ -17,7 +18,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             email: "user@example.com",
                                             password:              "password",
                                             password_confirmation: "password" }
-    assert_template 'users/new'
-  end
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
 end
 end
